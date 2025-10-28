fa071d28cc36073ff7eae87f43a21aff2701b61b
liyixin135 <421159734@qq.com>
2025-10-28 15:08:53 +0800

    Make a comment.

--- sunray_swarm/agent_control/rmtt_control_node.cpp
+++ sunray_swarm/agent_control/rmtt_control_node.cpp
@@ -13,17 +13,21 @@ int main(int argc, char **argv)
 {
     ros::init(argc, argv, "rmtt_control_node");
     ros::NodeHandle nh("~");
-    ros::Rate rate(20.0);
+    ros::Rate rate(20.0);//控制循环频率为 20 Hz
 
+    //可以在 Ctrl+C 时先做 清理工作（关闭文件、释放资源、停 ROS 节点）
     signal(SIGINT, mySigintHandler);
     ros::Duration(1.0).sleep();
 
     sleep(8.0);
 
     // 控制器
+    //在rmtt_control.h里面定义的，调用类
     RMTT_CONTROL rmtt_control;
+    //传入句柄，去获取参数，初始化发布者和订阅者、定时器
     rmtt_control.init(nh);
 
+    //ros::spinOnce() 在每轮循环中处理一次回调
     ros::spinOnce();
     ros::Duration(1.0).sleep();
 
@@ -33,6 +37,7 @@ int main(int argc, char **argv)
         // 回调函数
         ros::spinOnce();
         // 主循环函数
+        //调用类的函数，这里就是实现控制的关键
         rmtt_control.mainloop();
         // sleep
         rate.sleep();
